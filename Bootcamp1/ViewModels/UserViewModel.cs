4b458039d8fbaeafeead2a5c3ca0bb67e2145332
soetanto <c.soetantoaa@gmail.com>
2022-01-13 12:03:52 +0700

    day1. HT. konsep MVC + viewModel

--- Bootcamp1/ViewModels/UserViewModel.cs
+++ Bootcamp1/ViewModels/UserViewModel.cs
@@ -6,5 +6,8 @@ namespace Bootcamp1.ViewModels
     public class UserViewModel
     {
         public List<UserModel> Users { get; set; }
+        public BookModel Book { get; set; }
+        public UserModel User { get; set; }
+        public string Message { get; set; }
     }
 }
