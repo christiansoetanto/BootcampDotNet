cf265aac10be1116dd1f4428bd78b64d26f15754
Stephen <stephenleonardo18@gmail.com>
2022-01-13 14:18:08 +0700

    Feature to Create and Read data

---
+++ Bootcamp1/Controllers/FoodController.cs
@@ -0,0 +1,69 @@
+﻿using Bootcamp1.Models;
+using Bootcamp1.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootcamp1.Controllers
+{
+    public class FoodController : Controller
+    {
+
+        public static List<FoodModel> FoodList = new List<FoodModel>()
+        {
+            new FoodModel()
+            {
+                FoodID = 1,
+                FoodName = "Burger",
+                Price = 12.89f
+            },
+            new FoodModel()
+            {
+                FoodID = 2,
+                FoodName = "Pizza",
+                Price = 8.23f
+            },
+            new FoodModel()
+            {
+                FoodID = 3,
+                FoodName = "French Fries",
+                Price = 23.54f
+            }
+        };
+
+
+        public IActionResult Index()
+        {
+
+            
+
+
+            FoodViewModel FoodVM = new FoodViewModel();
+
+            FoodVM.FoodList = FoodList;
+
+
+            return View("Menu", FoodVM);
+        }
+
+
+
+        public IActionResult Create(FoodViewModel ModelSubmit)
+        {
+
+            // linQ
+            int NewID = FoodList.Max(e => e.FoodID) + 1;
+
+            ModelSubmit.Food.FoodID = NewID;
+
+            FoodList.Add(ModelSubmit.Food);
+
+
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
---
+++ Bootcamp1/Models/FoodModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootcamp1.Models
+{
+    public class FoodModel
+    {
+        public int FoodID { get; set; }
+        public string FoodName { get; set; }
+        public float Price { get; set; }
+    }
+}
